2a7299738ccc12d050c7a7071bea7b8b0c29b74d
renataalexandria <xnightsky2008@gmail.com>
2024-06-19 23:18:57 +0700

    update number format

--- frontend/src/mining.php
+++ frontend/src/mining.php
@@ -185,8 +185,8 @@ $best_attribute = '';
                         echo "<td>{$value['Exploits']}</td>";
                         echo "<td>{$value['Generic']}</td>";
                         echo "<td>{$value['Normal']}</td>";
-                        echo "<td>{$value['Entropy']}</td>";
-                        echo "<td>{$value['Gain']}</td>";
+                        echo "<td>" . number_format($value['Entropy'], 3) . "</td>";
+                        echo "<td>" . number_format($value['Gain'], 3) . "</td>";                        
                         echo '</tr>';
                     }
                     $total_gain_attribute = 0;
@@ -204,11 +204,11 @@ $best_attribute = '';
                     $gain_ratios[$feature] = $gain_ratio_total;
                 
                     echo '</table>';
-                    echo "<td>Gain : $total_gain</td>";
-                    echo '<br>';
-                    echo "<td>Split Ratios : {$total_split_info}</td>";
-                    echo '<br>';
-                    echo "<td>Gain Ratios : {$gain_ratio_total}</td>";
+                    echo "<td>Gain : " . number_format($total_gain, 3) . "</td>";
+                    echo "<br>";
+                    echo "<td>Split Ratios : " . number_format($total_split_info, 3) . "</td>";
+                    echo "<br>";
+                    echo "<td>Gain Ratios : " . number_format($gain_ratio_total, 3) . "</td>";
                     echo '<br>';
                     echo '<br>';
                 
@@ -216,204 +216,6 @@ $best_attribute = '';
                 }
                 // echo "The best attribute to split on is: $best_attribute";
                     echo "<h4>Values for Best Attribute ($best_attribute)</h4>";
-
-                
-                // Display another table for the best attribute
-                // if (!empty($best_attribute)) {
-                //     echo "<h4>Values for Best Attribute ($best_attribute)</h4>";
-                //     echo "<table border='1'>";
-                //     // echo "<tr><th>Value</th><th>Count</th></tr>";
-                
-                //     // Fetch unique values for the best attribute
-                //     $best_attribute_values = array_unique(array_column($data, $best_attribute));
-                //     foreach ($best_attribute_values as $value) {
-                //     }
-                
-                //     echo '</table>';
-                
-                //     $sqlp2 = "SELECT * FROM data_training where service = 'http'";
-                //     $result2 = $conn->query($sqlp2);
-                
-                //     // Persiapkan data untuk diproses oleh algoritma C4.5
-                //     $data2 = [];
-                //     if ($result2->num_rows > 0) {
-                //         while ($row = $result2->fetch_assoc()) {
-                //             $data2[] = $row;
-                //         }
-                //     }
-                
-                //     // Hitung jumlah rekaman total
-                //     $total_records = count($data2);
-                
-                //     // Hitung jumlah rekaman dalam setiap kelas
-                //     $class_counts = array_count_values(array_column($data2, 'attack_cat'));
-                
-                //     // Hitung entropi dataset
-                //     $entropy = 0;
-                //     foreach ($class_counts as $class_count) {
-                //         $probability = $class_count / $total_records;
-                //         $entropy -= $probability * log($probability, 2);
-                //     }
-                
-                //     $exploits_count = isset($class_counts['Exploits']) ? $class_counts['Exploits'] : 0;
-                //     $generic_count = isset($class_counts['Generic']) ? $class_counts['Generic'] : 0;
-                //     $fuzzers_count = isset($class_counts['Fuzzers']) ? $class_counts['Fuzzers'] : 0;
-                //     $normal_count = isset($class_counts['Normal']) ? $class_counts['Normal'] : 0;
-                
-                //     // Calculate the probability of each attack category
-                //     $exploits_probability = $exploits_count / $total_records;
-                //     $generic_probability = $generic_count / $total_records;
-                //     $fuzzers_probability = $fuzzers_count / $total_records;
-                //     $normal_probability = $normal_count / $total_records;
-                
-                //     // Calculate the entropy contribution for each attack category
-                //     $exploits_entropy = $exploits_count > 0 ? -$exploits_probability * log($exploits_probability, 2) : 0;
-                //     $generic_entropy = $generic_count > 0 ? -$generic_probability * log($generic_probability, 2) : 0;
-                //     $fuzzers_entropy = $fuzzers_count > 0 ? -$fuzzers_probability * log($fuzzers_probability, 2) : 0;
-                //     $normal_entropy = $normal_count > 0 ? -$normal_probability * log($normal_probability, 2) : 0;
-                
-                //     // Total entropy calculation
-                //     $total_entropy = $exploits_entropy + $generic_entropy + $fuzzers_entropy + $normal_entropy;
-                
-                //     // Hitung statistik untuk setiap fitur
-                //     $statistics = [
-                //         'service' => [],
-                //         'spkts' => [],
-                //         'sbytes' => [],
-                //         'sttl' => [],
-                //         'smean' => [],
-                //         'null' => [],
-                //     ];
-                
-                //     foreach ($statistics as $feature => &$values) {
-                //         $total_gain_attribute = 0;
-                //         $unique_values = array_unique(array_column($data, $feature));
-                //         if ($feature == 'spkts') {
-                //             // Kelompokkan spkts menjadi <=12 dan >12
-                //             $unique_values = ['<=12', '>12'];
-                //         } elseif ($feature == 'sbytes') {
-                //             // Kelompokkan sbytes menjadi <=766 dan >766
-                //             $unique_values = ['<=826', '>826'];
-                //         } elseif ($feature == 'sttl') {
-                //             // Kelompokkan sttl menjadi 31, 62, dan 254
-                //             $unique_values = [31, 62, 254];
-                //         } elseif ($feature == 'smean') {
-                //             // Kelompokkan smean menjadi <=78 dan >78
-                //             $unique_values = ['<=78', '>78'];
-                //         } else {
-                //             $unique_values = array_unique(array_column($data, $feature));
-                //         }
-                
-                //         foreach ($unique_values as $value) {
-                //             if ($feature === 'null') {
-                //                 continue; // Skip null feature
-                //             }
-                
-                //             $subset_entropy = 0;
-                //             $value_count = 0;
-                //             $class_counts_subset = [];
-                //             $split_info_subset = 0;
-                
-                //             foreach ($data as $row) {
-                //                 $match_condition = false;
-                //                 if ($feature == 'spkts') {
-                //                     $match_condition = ($value == '<=12' && $row[$feature] <= 12) || ($value == '>12' && $row[$feature] > 12);
-                //                 } elseif ($feature == 'sbytes') {
-                //                     $match_condition = ($value == '<=826' && $row[$feature] <= 826) || ($value == '>826' && $row[$feature] > 826);
-                //                 } elseif ($feature == 'sttl') {
-                //                     $match_condition = $row[$feature] == $value;
-                //                 } elseif ($feature == 'smean') {
-                //                     $match_condition = ($value == '<=78' && $row[$feature] <= 78) || ($value == '>78' && $row[$feature] > 78);
-                //                 } else {
-                //                     $match_condition = $row[$feature] == $value;
-                //                 }
-                
-                //                 if ($match_condition) {
-                //                     $value_count++;
-                //                     if (!isset($class_counts_subset[$row['attack_cat']])) {
-                //                         $class_counts_subset[$row['attack_cat']] = 0;
-                //                     }
-                //                     $class_counts_subset[$row['attack_cat']]++;
-                //                 }
-                //             }
-                
-                //             $gain = 0;
-                //             foreach ($class_counts_subset as $class_count) {
-                //                 $probability = $class_count / $value_count;
-                //                 $subset_entropy -= $probability * log($probability, 2);
-                
-                //                 $subset_probability = $value_count / $total_records;
-                
-                //                 $gain_subset = $subset_probability * $subset_entropy;
-                //                 $gain += $gain_subset;
-                //             }
-                
-                //             $split_info_subset -= $subset_probability * log($subset_probability, 2);
-                
-                //             $values[] = [
-                //                 'Value' => $value,
-                //                 'Jml_Record' => $value_count,
-                //                 'Fuzzers' => isset($class_counts_subset['Fuzzers']) ? $class_counts_subset['Fuzzers'] : 0,
-                //                 'Exploits' => isset($class_counts_subset['Exploits']) ? $class_counts_subset['Exploits'] : 0,
-                //                 'Generic' => isset($class_counts_subset['Generic']) ? $class_counts_subset['Generic'] : 0,
-                //                 'Normal' => isset($class_counts_subset['Normal']) ? $class_counts_subset['Normal'] : 0,
-                //                 'Entropy' => $subset_entropy,
-                //                 'Gain' => $gain_subset,
-                //                 'Split_Info' => $split_info_subset,
-                //             ];
-                //         }
-                //     }
-                
-                //     $gain_ratios = [];
-                //     $best_attribute = '';
-
-                //     foreach ($statistics as $feature => $values) {
-                //         if ($feature === 'null') {
-                //             continue; // Skip null feature
-                //         }
-                //         echo "<h6 class='mb-0 text-uppercase'>$feature</h6>";
-                //         echo "<table border='1'>";
-                //         echo '<tr><th>Value</th><th>Jml Record</th><th>Fuzzers</th><th>Exploits</th><th>Generic</th><th>Normal</th><th>Entropy</th><th>Gain</th></tr>';
-                //         foreach ($values as $value) {
-                //             echo '<tr>';
-                //             echo "<td>{$value['Value']}</td>";
-                //             echo "<td>{$value['Jml_Record']}</td>";
-                //             echo "<td>{$value['Fuzzers']}</td>";
-                //             echo "<td>{$value['Exploits']}</td>";
-                //             echo "<td>{$value['Generic']}</td>";
-                //             echo "<td>{$value['Normal']}</td>";
-                //             echo "<td>{$value['Entropy']}</td>";
-                //             echo "<td>{$value['Gain']}</td>";
-                //             echo '</tr>';
-                //         }
-                //         $total_gain_attribute = 0;
-                //         $total_split_info = 0;
-                    
-                //         // Calculate total gain for this attribute
-                //         foreach ($values as $value) {
-                //             $total_gain_attribute += $value['Gain'];
-                //             $total_split_info += $value['Split_Info'];
-                //         }
-                //         $total_gain = $total_entropy - $total_gain_attribute;
-                    
-                //         $gain_ratio_total = $total_gain / $total_split_info;
-                    
-                //         $gain_ratios[$feature] = $gain_ratio_total;
-                    
-                //         echo '</table>';
-                //         echo "<td>Gain : $total_gain</td>";
-                //         echo '<br>';
-                //         echo "<td>Split Ratios : {$total_split_info}</td>";
-                //         echo '<br>';
-                //         echo "<td>Gain Ratios : {$gain_ratio_total}</td>";
-                //         echo '<br>';
-                //         echo '<br>';
-                    
-                //         $best_attribute = array_search(max($gain_ratios), $gain_ratios);
-                //     }
-                //     echo "The best attribute to split on is: $best_attribute";
-                    
-                // }
                 
                 ?>
                 <!-- <table id="example" class="table table-striped table-bordered" style="width:100%">
